ffba1b856a5e3f70e319d4e4918cac7d9fbca19d
Brandon Louis Gusto <blg13@dskscs012.sc.local>
2018-03-09 12:18:45 -0500

    papers

--- test/circumtest.m
+++ test/circumtest.m
@@ -2,12 +2,12 @@ clc, clear all, clf
 
 % define triangle vertices
 x = [0 .5 1];
-y = [0 2 .2];
+y = [0 1 0];
 cor = [x;y];
 
 v1 = [0 0];
-v2 = [.5 2];
-v3 = [1 .2];
+v2 = [.5 1];
+v3 = [1 0];
 
 [x y] = circumcenter(v1,v2,v3)
 
